1579498340b16b5742c9c328b761300a24735777
Piklite <gregoryds14@gmail.com>
2018-04-23 12:03:16 +0200

    Lesson 3 - Creating a Textfield inside UIAlert to add new items / Add new item functionality complete.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,18 +10,19 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     //MARK - Tableview Datasource Methods
+    // Declare the number of rows in table view
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    // Create a cell and populate it with data for each row
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
@@ -29,21 +30,49 @@ class TodoListViewController: UITableViewController {
     }
     
     //MARK - TableView Delegate Methods
-    
+    // Row selection
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
-        
+        // Add accessory checkmark
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
+        // Deselect automaticly for better UI
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        
-        
     }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //Adding a global variable to extend the scope of alertTextField
+        var textField = UITextField()
+        
+        //Create an alert (popup)
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        //Create an action (button) for the alerte popup
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //Append the new item to the array
+            self.itemArray.append(textField.text!)
+            
+            //Reload the tableview to update the UI with the new data
+            self.tableView.reloadData()
+        }
+        
+        //Adding a text field in the alert popup
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        //Add the action to the alerte
+        alert.addAction(action)
+        
+        //Present the alert when the + button is pressed
+        present(alert, animated: true, completion: nil)
+    }
+    
     
     
 }
